72fdda55134a898780bee6161d4bf61487d117e7
weaseldn <chrisducnguyen04@gmail.com>
2025-10-20 09:09:49 +0700

    Add files via upload

---
+++ NguyenTranHuuDuc_1150080049_BTtuan8/NguyenTranHuuDuc_1150080049_BTtuan8/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace NguyenTranHuuDuc_1150080049_BTtuan8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lsvDanhSach = new System.Windows.Forms.ListView();
+            this.MaNXB = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TenNXB = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DiaChi = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtTenNXB = new System.Windows.Forms.TextBox();
+            this.txtMaNXB = new System.Windows.Forms.TextBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(335, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thêm dữ liệu";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lsvDanhSach);
+            this.groupBox1.Location = new System.Drawing.Point(62, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 337);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách nhà xuất bản";
+            // 
+            // lsvDanhSach
+            // 
+            this.lsvDanhSach.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.MaNXB,
+            this.TenNXB,
+            this.DiaChi});
+            this.lsvDanhSach.HideSelection = false;
+            this.lsvDanhSach.Location = new System.Drawing.Point(6, 35);
+            this.lsvDanhSach.Name = "lsvDanhSach";
+            this.lsvDanhSach.Size = new System.Drawing.Size(337, 283);
+            this.lsvDanhSach.TabIndex = 0;
+            this.lsvDanhSach.UseCompatibleStateImageBehavior = false;
+            this.lsvDanhSach.View = System.Windows.Forms.View.Details;
+            // 
+            // MaNXB
+            // 
+            this.MaNXB.Text = "Mã NXB";
+            // 
+            // TenNXB
+            // 
+            this.TenNXB.Text = "Tên NXB";
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.Text = "Địa chỉ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCapNhat);
+            this.groupBox2.Controls.Add(this.txtDiaChi);
+            this.groupBox2.Controls.Add(this.txtTenNXB);
+            this.groupBox2.Controls.Add(this.txtMaNXB);
+            this.groupBox2.Controls.Add(this.btnThem);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(456, 87);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 337);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thông tin nhập liệu";
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(95, 184);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(191, 22);
+            this.txtDiaChi.TabIndex = 6;
+            // 
+            // txtTenNXB
+            // 
+            this.txtTenNXB.Location = new System.Drawing.Point(95, 119);
+            this.txtTenNXB.Name = "txtTenNXB";
+            this.txtTenNXB.Size = new System.Drawing.Size(191, 22);
+            this.txtTenNXB.TabIndex = 5;
+            // 
+            // txtMaNXB
+            // 
+            this.txtMaNXB.Location = new System.Drawing.Point(95, 49);
+            this.txtMaNXB.Name = "txtMaNXB";
+            this.txtMaNXB.Size = new System.Drawing.Size(191, 22);
+            this.txtMaNXB.TabIndex = 4;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.Lime;
+            this.btnThem.Location = new System.Drawing.Point(67, 221);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(176, 33);
+            this.btnThem.TabIndex = 3;
+            this.btnThem.Text = "Thêm nhà xuất bản";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(24, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Địa chỉ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(24, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tên NXB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mã NXB";
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Location = new System.Drawing.Point(67, 260);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(176, 23);
+            this.btnCapNhat.TabIndex = 7;
+            this.btnCapNhat.Text = "Cập nhật thông tin";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtTenNXB;
+        private System.Windows.Forms.TextBox txtMaNXB;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lsvDanhSach;
+        private System.Windows.Forms.ColumnHeader MaNXB;
+        private System.Windows.Forms.ColumnHeader TenNXB;
+        private System.Windows.Forms.ColumnHeader DiaChi;
+        private System.Windows.Forms.Button btnCapNhat;
+    }
+}
+
---
+++ NguyenTranHuuDuc_1150080049_BTtuan8/NguyenTranHuuDuc_1150080049_BTtuan8/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace NguyenTranHuuDuc_1150080049_BTtuan8
+{
+    public partial class Form1 : Form
+    {
+    
+        string strCon = @"Data Source=(LocalDB)\MSSQLLocalDB;
+        AttachDbFilename=C:\Users\Duc\Documents\Nam 4\HK1\PP PTPM HDT\NguyenTranHuuDuc_1150080049_BTtuan8\NguyenTranHuuDuc_1150080049_BTtuan8\QuanLyBanSach.mdf;
+        Integrated Security=True";
+
+        SqlConnection sqlCon = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 🔹 Mở kết nối
+        private void MoKetNoi()
+        {
+            if (sqlCon == null)
+                sqlCon = new SqlConnection(strCon);
+            if (sqlCon.State == ConnectionState.Closed)
+                sqlCon.Open();
+        }
+
+        // 🔹 Đóng kết nối
+        private void DongKetNoi()
+        {
+            if (sqlCon != null && sqlCon.State == ConnectionState.Open)
+                sqlCon.Close();
+        }
+
+        // 🔹 Hiển thị danh sách NXB (gọi thủ tục "HienThiNXB")
+        private void HienThiDanhSachXB()
+        {
+            MoKetNoi();
+            SqlCommand sqlCmd = new SqlCommand();
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.CommandText = "HienThiNXB";
+            sqlCmd.Connection = sqlCon;
+
+            SqlDataReader reader = sqlCmd.ExecuteReader();
+            lsvDanhSach.Items.Clear();
+
+            while (reader.Read())
+            {
+                string maXB = reader.GetString(0);
+                string tenXB = reader.GetString(1);
+                string diaChi = reader.GetString(2);
+
+                ListViewItem lvi = new ListViewItem(maXB);
+                lvi.SubItems.Add(tenXB);
+                lvi.SubItems.Add(diaChi);
+
+                lsvDanhSach.Items.Add(lvi);
+            }
+
+            reader.Close();
+            DongKetNoi();
+        }
+
+        // 🔹 Khi form mở, tự động hiển thị danh sách
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThiDanhSachXB();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            MoKetNoi();
+            SqlCommand sqlCmd = new SqlCommand("ThemDuLieu", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+
+            sqlCmd.Parameters.AddWithValue("@NXB", txtMaNXB.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@TenNXB", txtTenNXB.Text.Trim());
+            sqlCmd.Parameters.AddWithValue("@DiaChi", txtDiaChi.Text.Trim());
+
+            int kq = sqlCmd.ExecuteNonQuery();
+            if (kq > 0)
+            {
+                MessageBox.Show("Thêm dữ liệu thành công!");
+                HienThiDanhSachXB();
+                txtMaNXB.Clear(); txtTenNXB.Clear(); txtDiaChi.Clear();
+            }
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            if (lsvDanhSach.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Chọn một dòng trong danh sách để sửa.");
+                return;
+            }
+
+            try
+            {
+                MoKetNoi();
+                using (var sqlCmd = new SqlCommand("dbo.SuaDuLieu", sqlCon))
+                {
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.Add("@NXB", SqlDbType.Char, 10).Value = txtMaNXB.Text.Trim();
+                    sqlCmd.Parameters.Add("@TenNXB", SqlDbType.NVarChar, 100).Value = txtTenNXB.Text.Trim();
+                    sqlCmd.Parameters.Add("@DiaChi", SqlDbType.NVarChar, 500).Value = txtDiaChi.Text.Trim();
+                    sqlCmd.ExecuteNonQuery();
+                }
+
+                MessageBox.Show("Cập nhật thành công!");
+                HienThiDanhSachXB();
+                // chọn lại dòng vừa sửa (tùy chọn)
+                foreach (ListViewItem it in lsvDanhSach.Items)
+                    if (it.SubItems[0].Text.Equals(txtMaNXB.Text.Trim(), StringComparison.OrdinalIgnoreCase))
+                    { it.Selected = true; it.EnsureVisible(); break; }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("SQL error: " + ex.Message);
+            }
+            finally
+            {
+                DongKetNoi();
+            }
+        }
+    }
+}
